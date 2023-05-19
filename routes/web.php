b48d7495c2341133ea8e44e9756816f8a127cb81
ab <akhmadovdev@gmail.com>
2023-05-19 21:13:19 +0500

    install template, url route, navbar translate uzbek language

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'main']);
+Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('services', [PageController::class, 'services'])->name('services');
+
+
