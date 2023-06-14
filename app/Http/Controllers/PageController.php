9b68e395364cfa3794e7f523a1724c95a89e8d65
ab <akhmadovdev@gmail.com>
2023-06-14 11:18:45 +0500

    new commit

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,4 +20,14 @@ class PageController extends Controller
     {
         return view('services');
     }
+
+    public function projects()
+    {
+        return view('projects');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
 }
